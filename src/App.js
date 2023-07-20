0925f87333c434f0dd8cf4eec1a6a7cc2c366b4c
sai3454 <saikrishnakoorakula7@gmail.com>
2023-07-20 15:30:13 +0530

    fetched and displayed images

--- src/App.js
+++ src/App.js
@@ -1,25 +1,66 @@
-import logo from './logo.svg';
+import {Component} from 'react'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+class App extends Component{
+  state = {
+    imagesList: []
+  }
+
+  
+
+  getAllImages = async () => {
+    
+    const apiUrl = `https://api.unsplash.com/photos/?client_id=R7iDpKlHA0aL3SM26DcQxl7jiHFlHe_giA3nrzexLaM`
+
+    const res = await fetch(apiUrl)
+    const data = await res.json() 
+    console.log("yes")
+
+    if(res.status === 200){
+      this.setState({imagesList: data}) 
+    }
+  }
+
+
+  componentDidMount = () => {
+    this.getAllImages();
+  }
+
+  render(){
+    const {searchInput, imagesList} = this.state
+
+
+    return(
+      <div className="contaienr">
+        
+        
+        
+            <div className='center'>
+            <ul className='images-container'>
+              {imagesList.map(eachImage => {
+                const {id, urls, description} = eachImage
+                const {small} = urls
+                
+                return (
+                  <li key={eachImage.id}>
+                    <img 
+                      src={small} 
+                      alt="search" 
+                      className='image'
+                      
+                    />
+                  </li>
+                )
+              })}
+            </ul>
+            
+            </div>
+          
+      </div>
+    )
+  }
 }
 
 export default App;
